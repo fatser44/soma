7245c47dc7ac17081c2496b7e31fb45b9a01a7c0
fatser44 <32727333+fatser44@users.noreply.github.com>
2017-10-12 10:26:39 +0200

    Add files via upload

---
+++ kk/test.js
@@ -0,0 +1,17 @@
+var sam2;
+
+sam2=function(){
+	"use strict";
+	var d2,
+s2;
+	
+	
+	s2.innerHTML="error";
+
+	
+	d2= document.getElementById("x2");
+	s2=document.getElementById("in2");
+	d2.innerHTML="<center>"+"<font size=5  id='fo'>"+s2.value;
+	
+	
+};
---
+++ kk/test.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<script src="test.js" type="text/jscript"></script>
+</head>
+
+<body>
+<button onClick="sam2()">check up</button>
+<input type="text" id="in2">
+<br><br><br>
+<br><div id="x2"></div>
+</body>
+</html>
